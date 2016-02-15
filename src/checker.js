392705dfa8c692a7f4f4b1c0e3728c7fbb6c4249
xgy <xgy1991@gmail.com>
2016-02-15 18:27:52 -0500

    Try with Atd example page

--- src/checker.js
+++ src/checker.js
@@ -1,3 +1,26 @@
 /**
+ *
  * Created by xgy on 15/02/16.
  */
+
+//var checkButton = document.getElementById("checkbutton");
+//var textbox = document.getElementById("checkinput");
+//
+//checkButton.addEventListener("click", function (event) {
+//});
+
+//textbox.addEventListener("keypress", function (event) {
+//    if (event.keyCode == 13) {
+//        triggerSearch(textbox);
+//    }
+//});
+//jQuery(function() {
+//		AtD.rpc_css = 'http://www.polishmywriting.com/dev/atd-jquery/server/proxycss.php?data=';
+//		$('textarea').addProofreader();
+//	});
+
+//function triggerSearch(textbox) {
+//    console.log(textbox.value);
+//    var string = textbox.value;
+//
+//}
